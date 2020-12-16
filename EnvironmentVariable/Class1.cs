9d0048da629d2b244532292190c01dfd91128cb8
xoerturk <53426590+erturkonur@users.noreply.github.com>
2020-12-16 14:38:53 +0300

    Add files via upload

---
+++ EnvironmentVariable/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace EnvironmentVariable
+{
+    public class GetEnvironmentVariable : CodeActivity
+    {
+
+        [Category("Input")]
+        public InArgument<string> Variable { get; set; }
+        [Category("Output")]
+        public OutArgument<string> ResultVariable { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string name = Environment.GetEnvironmentVariable(Variable.Get(context));
+
+            string result = name;
+
+            ResultVariable.Set(context, result);
+
+        }
+
+
+    }
+
+    public class SetEnvironmentVariable : CodeActivity
+    {
+        [Category("Input")]
+        public InArgument<string> Variable { get; set; }
+        [Category("Input")]
+        public InArgument<string> ResultVariable { get; set; }
+        
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            Environment.SetEnvironmentVariable(Variable.Get(context), ResultVariable.Get(context));
+
+            string result = Environment.GetEnvironmentVariable(Variable.Get(context));
+
+
+        }
+
+
+    }
+
+}
+
+    
